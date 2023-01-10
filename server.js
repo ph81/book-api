03379563e25b4787710865b7a0302f507bbb7f83
Edith M <paperhearts81@gmail.com>
2023-01-10 10:50:08 -0600

    chore: initial config

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// Import express
-let express = require('express');
-// Import Body parser
-let bodyParser = require('body-parser');
-// Import Mongoose
-let mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-let cors = require('cors');
+const cors = require('cors');
 require('dotenv').config();
 const dbUrl = process.env.MONGO_URI;
 // Initialize the app
