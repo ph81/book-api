c879c9deac92d010d8d69731bf071df84e8e741f
Edith <paperhearts81@gmail.com>
2023-01-11 19:25:07 -0600

    chore: model updated

--- bookModel.js
+++ bookModel.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 // Setup schema
 const bookSchema = mongoose.Schema({
+     id: {
+        type: String,
+        required: true
+    },
     title: {
         type: String,
         required: true
