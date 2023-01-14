d46053a0ef69d8ff2e3520f79fddd4dc3bd903d8
Edith <paperhearts81@gmail.com>
2023-01-14 17:45:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,56 +1,69 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-require('dotenv').config();
+// Import express
+let express = require("express");
+// Import Body parser
+let bodyParser = require("body-parser");
+// Import Mongoose
+let mongoose = require("mongoose");
+const dotenv = require("dotenv");
+//let cors = require("cors");
+require("dotenv").config();
 const dbUrl = process.env.MONGO_URI;
 // Initialize the app
-const app = express();
+let app = express();
 
 //enables cors
-app.use(cors({
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}));
+/*
+app.use(
+  cors({
+    allowedHeaders: ["sessionId", "Content-Type"],
+    exposedHeaders: ["sessionId"],
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
+*/
+
+app.all('*', function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 
 // Import routes
-let apiRoutes = require("./api-routes")
-
+let apiRoutes = require("./api-routes");
 
 // Configure bodyparser to handle post requests
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
 // Connect to database
 // Connect to Mongoose and set connection variable
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 mongoose.Promise = global.Promise;
 mongoose.connection
-  .on('connected', () => {
-    console.log(`Mongoose connection open`);
+  .on("connected", () => {
+    console.log(`Mongoose connection open on ${dbUrl}`);
   })
-  .on('error', (err) => {
+  .on("error", (err) => {
     console.log(`Connection error: ${err.message}`);
   });
 
-//var db = mongoose.connection;
 // Setup server port
-var port = process.env.PORT || 8080;
+let port = process.env.PORT || 8080;
 
 // Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express & Vercel'));
+app.get("/", (req, res) => res.send("Hello World with Express"));
 
 // Use Api routes in the App
-app.use('/api', apiRoutes)
+app.use("/api", apiRoutes);
 
 // Launch app to listen to specified port
 app.listen(port, function () {
-    console.log("Running server on port " + port);
+  console.log("Running server on port " + port);
 });
