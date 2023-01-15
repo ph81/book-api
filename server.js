aeb563ede8c0e8306170d0de1e24042d657cf9aa
Edith <paperhearts81@gmail.com>
2023-01-15 01:24:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
   'exposedHeaders': ['sessionId'],
   'origin': '*',
   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
+  'preflightContinue': true
 }));
 
 // Import routes
